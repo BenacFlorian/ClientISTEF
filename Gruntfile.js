42c4bf0723265e48bb15a7f515ea2b2098d1dbe8
Florian Benac <florian.benac@webinage.fr>
2017-02-13 18:32:47 +0100

    Config grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -144,26 +144,6 @@ module.exports = function (grunt) {
                 }
             }
         },
-
-        // Make sure code styles are up to par and there are no obvious mistakes
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            all: {
-                src: [
-          'Gruntfile.js',
-          '<%= yeoman.app %>/scripts/{,*/}*.js'
-        ]
-            },
-            test: {
-                options: {
-                    jshintrc: 'test/.jshintrc'
-                },
-                src: ['test/spec/{,*/}*.js']
-            }
-        },
         // configure compress to tar the whole project
         compress: {
             main: {
@@ -482,24 +462,18 @@ module.exports = function (grunt) {
     grunt.registerTask('build', [
     'clean:dist',
     'wiredep',
-    'useminPrepare',
     'concurrent:dist',
-    'autoprefixer',
-    'ngtemplates',
     'concat',
     'ngAnnotate',
-    'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'filerev',
     'usemin',
-    'htmlmin'
+    'htmlmin'    
   ]);
 
     grunt.registerTask('default', [
-    'newer:jshint',
-    'test',
-    'build'
+    'build',
+    'compress'
   ]);
 };
